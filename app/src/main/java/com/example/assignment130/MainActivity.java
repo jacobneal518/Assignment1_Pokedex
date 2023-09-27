40165f4f0dc0654cc68a7e143fa08d4a57cf652e
Jacob Neal <jacob@binaryevolution.com>
2023-09-26 21:28:56 -0500

    added table and constraint layout

--- app/src/main/java/com/example/assignment130/MainActivity.java
+++ app/src/main/java/com/example/assignment130/MainActivity.java
@@ -120,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.linear);
+        setContentView(R.layout.constraint);
 
         //region variable initialization
         nameInput = findViewById(R.id.nameInput);
